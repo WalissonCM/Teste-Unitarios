287f7e5662076fce91d9c8cb2b2c42c3a6b3268a
WalissonCM <walissoncavalcante@outlook.com>
2023-05-15 19:52:30 -0300

    test

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+function soma(a, b){
+    return a+b
+}
+
+
+function multiplicar(a, b){
+    return a*b
+}
+
+
+function subtrair(a, b){
+    return a-b
+}
+
+
+function dividir(a, b){
+    return a/b
+}
+
+exports.soma = soma
+exports.multiplicar = multiplicar
+exports.dividir = dividir
+exports.subtrair = subtrair
---
+++ calculadora.test.js
@@ -0,0 +1,16 @@
+const {createTestScheduler} = require ('jest')
+
+
+const calculo = require('./calculadora')
+
+describe('funcões matemáticas', ()=>{
+  
+    test('Soma dois numeros', ()=>{
+
+        expect(calculo.soma(10,20)).toBe(30)
+
+        expect(calculo.soma(20,20)).toBe(40)
+
+} )
+
+} )
