3d128f7d5e6e0ee30f666750a86a10f52e4423b4
WalissonCM <walissoncavalcante@outlook.com>
2023-05-22 21:20:57 -0300

    test

---
+++ db/media.js
@@ -0,0 +1,7 @@
+const soma = require('./soma')
+function media(a, b){
+s = soma(a, b)
+m = s/2
+return m
+}
+exports.media = media
---
+++ db/media.test.js
@@ -0,0 +1,6 @@
+const calculo = require('./media')
+
+test('Testando a media aritmética', () => {
+
+    expect(calculo.media(7, 4)).toBe(5.5)
+})
